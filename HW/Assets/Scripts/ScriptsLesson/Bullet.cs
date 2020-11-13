146139e97411422e465f9dc1359a0d97025af538
Papowe <63293783+Papowe@users.noreply.github.com>
2020-11-13 11:56:33 +0200

    Добавил стрельбу по врагам.

---
+++ HW/Assets/Scripts/ScriptsLesson/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private int damage = 50;
+    private float speedBullet = 10f;
+
+    public int Damage { get => damage; set => damage = value; }
+
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speedBullet * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Wall"))
+        {
+            other.gameObject.GetComponent<Enemy>()?.GetDamage(damage);
+            Destroy(gameObject);
+        }        
+    }
+}
---
+++ HW/Assets/Scripts/ScriptsLesson/Shot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    [SerializeField] private int damageBullet;
+    [SerializeField] private Transform bulletDeparturePoint;
+    [SerializeField] private GameObject prefabBullet;    
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projecTille = Instantiate(prefabBullet, bulletDeparturePoint.position, bulletDeparturePoint.rotation);
+            projecTille.GetComponent<Bullet>().Damage = damageBullet;
+        }
+    }
+}
